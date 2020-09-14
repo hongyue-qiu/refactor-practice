143f7899dfef974d82a5dc6dae4a13479d307e47
hongyue-qiu <994059641@qq.com>
2020-09-15 00:50:11 +0800

    [qiuhongyue] finsh refactor practice

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -27,13 +27,17 @@ public class Receipt {
         int totalKms = taxi.getTotalKms();
         double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost = getTotalCost(totalCost, totalKms, peakTimeMultiple,PRE_RATE_CHANGE_AC_RATE);
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost = getTotalCost(totalCost, totalKms, peakTimeMultiple,POST_RATE_CHANGE_NON_AC_RATE);
         }
 
         return totalCost * (1 + SALES_TAX_RATE);
+    }
+
+    public double getTotalCost(double totalCost, int totalKms, double peakTimeMultiple,int preRateChangesAcRate) {
+        totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * preRateChangesAcRate * peakTimeMultiple;
+        totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * preRateChangesAcRate * peakTimeMultiple;
+        return totalCost;
     }
 }
